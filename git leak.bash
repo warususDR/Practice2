8ac21f82138bad38fe25029998052d7b32bf24ef
warususDR <97735199+warususDR@users.noreply.github.com>
2022-07-14 21:11:48 +0300

    added leak test

---
+++ git leak.bash
@@ -0,0 +1,11 @@
+{
+   "line": "const AWS_KEY = \"AKIALALEMEL33243OLIAE\"",
+   "commit": "1310d0e0c6d41ff4df89d3bf9fe0cf428932b1eb",
+   "offender": "AKIALALEMEL33243OLIA",
+   "reason": "AWS",
+   "commitMsg": "Add keys ",
+   "author": "Umut Seven \u003cumutseven92@gmail.com\u003e",
+   "file": "keys.go",
+   "repo": ".",
+   "date": "2019-05-10T01:33:39+01:00"
+}
